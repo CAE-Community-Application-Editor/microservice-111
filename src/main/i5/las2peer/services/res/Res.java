767a6517d127c9b5b396f25cf851f37338fcae33
CAEUser <mail@rwth>
2021-09-15 18:28:29 +0000

    Http Response[response] edited by Anonymous

--- src/main/i5/las2peer/services/res/Res.java
+++ src/main/i5/las2peer/services/res/Res.java
@@ -120,14 +120,14 @@ public class Res extends RESTService {
  
         json.put("name", "student");
 
-JSONArray array = new JSONArray();
-JSONObject item = new JSONObject();
-item.put("information", "test");
-item.put("id", 3);
-item.put("name", "course1");
-array.add(item);
-
-json.put("course", array);
+        JSONArray array = new JSONArray();
+        JSONObject item = new JSONObject();
+        item.put("information", "test");
+        item.put("id", 3);
+        item.put("name", "course1");
+        array.add(item);
+        
+        json.put("course", array);
 
       return Response.status(HttpURLConnection.HTTP_OK).entity(result.toJSONString()).build(); 
     }
